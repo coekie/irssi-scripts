677d63c68c62fe02bacb5de80710af3fc7062db5
Wouter Coekaerts <wouter@coekaerts.be>
2005-09-06 14:10:27 +0000

    make the code look a bit more sane
    git-svn-id: file:///home/coekie/subversion/coekie/irssi/scripts/trunk@58 8b93cb70-b3e5-0310-902c-ff4336cc7ade

--- printlevels.pl
+++ printlevels.pl
@@ -1,21 +1,19 @@
 use Irssi;
 use strict;
 use vars qw($VERSION %IRSSI);
-$VERSION = '1.0';
+$VERSION = '1.1';
 %IRSSI = (
 	authors     => 'Wouter Coekaerts',
-	contact     => "wouter\@coekaerts.be",
+	contact     => 'wouter@coekaerts.be',
 	name        => 'printlevels',
-	description => 'prints the message level before every line. usefull for debugging your level settings',
+	description => 'prints the message level before every line. useful for debugging your level settings',
 	license     => 'GPLv2',
 	url         => 'http://wouter.coekaerts.be/irssi',
 	changed     => '09/09/2003'
 );
 
-Irssi::signal_add('print text',\&sig_print_text);
-
-sub sig_print_text {
+Irssi::signal_add('print text', sub {
 	my ($dest, $text, $stripped) = @_;
-	@_[1] = '|' . Irssi::bits2level($dest->{'level'}) . '| ' . @_[1];
+	$_[1] = '|' . Irssi::bits2level($dest->{'level'}) . '| ' . $text;
 	Irssi::signal_continue(@_);
-}
+});
