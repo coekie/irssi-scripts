530ff9b86636c8dccb502ccc4aed3a5e65afaa03
Wouter Coekaerts <wouter@coekaerts.be>
2009-05-16 10:49:53 +0000

    Handle right, left and middle button separately. Add settings for clicks. Add command to temporarily disable mouse handling (instead of hardcoded)
    git-svn-id: file:///home/coekie/subversion/coekie/irssi/scripts/trunk@200 8b93cb70-b3e5-0310-902c-ff4336cc7ade

--- mouse.pl
+++ mouse.pl
@@ -11,11 +11,13 @@ $VERSION = '0.1.0';
 	authors  	=> 'Wouter Coekaerts',
 	contact  	=> 'wouter@coekaerts.be',
 	name    	=> 'mouse',
-	description 	=> 'experimental perl version of the irssi mouse patch',
+	description 	=> 'control irssi using mouse clicks and gestures',
 	license 	=> 'GPLv2',
 	url     	=> 'http://wouter.coekaerts.be/irssi/',
 	changed  	=> '2009-05-16',
 );
+
+my @BUTTONS = ('', '_middle', '_right');
 
 my $mouse_xterm_status = -1; # -1:off 0,1,2:filling mouse_xterm_combo
 my @mouse_xterm_combo = (3, 0, 0); # 0:button 1:x 2:y
@@ -27,7 +29,6 @@ sub mouse_enable {
 
 sub mouse_disable {
 	print STDERR "\e[?1000l"; # stop tracking 
-	Irssi::timeout_add_once(2000, 'mouse_enable', undef); # turn back on after 2 sec
 }
 
 # Handle mouse event (button press or release)
@@ -46,16 +47,16 @@ sub mouse_event {
 	# proceed only if a button is being released
 	return if ($b != 3);
 
+	return unless (0 <= $oldb && $oldb <= 2);
+	my $button = $BUTTONS[$oldb];
+
 	# if it was a mouse click of the left button (press and release in the same position)
-	if ($x == $oldx && $y == $oldy && $oldb == 0) {
-		#signal_emit("mouse click", $oldb, $x, $y);
-		#mouse_click($oldb, $x, $y);
-		mouse_disable();
+	if ($x == $oldx && $y == $oldy) {
+		cmd("mouse" . $button . "_click");
 		return;
 	}
 
-	# otherwise, find mouse gestures on button
-	return if ($oldb != 2);
+	# otherwise, find mouse gestures
 	$xd = $x - $oldx;
 	$yd = -1 * ($y - $oldy);
 	$distance = sqrt($xd*$xd + $yd*$yd);
@@ -66,23 +67,24 @@ sub mouse_event {
 	$angle = asin($yd/$distance) * 180 / 3.14159265358979;
 	if ($angle < 20 && $angle > -20 && $xd > 0) {
 		if ($distance <= 40) {
-			cmd("mouse_gesture_right");
+			cmd("mouse" . $button . "_gesture_right");
 		} else {
-			cmd("mouse_gesture_bigright");
+			cmd("mouse" . $button . "_gesture_bigright");
 		}
 	} elsif ($angle < 20 && $angle > -20 && $xd < 0) {
 		if ($distance <= 40) {
-			cmd("mouse_gesture_left");
+			cmd("mouse" . $button . "_gesture_left");
 		} else {
-			cmd("mouse_gesture_bigleft");
+			cmd("mouse" . $button . "_gesture_bigleft");
 		}
 	} elsif ($angle > 40) {
-		cmd("mouse_gesture_up");
+		cmd("mouse" . $button . "_gesture_up");
 	} elsif ($angle < -40) {
-		cmd("mouse_gesture_down");
+		cmd("mouse" . $button . "_gesture_down");
 	}
 }
 
+# executes the command configured in the given setting
 sub cmd
 {
 	my ($setting) = @_;
@@ -103,7 +105,6 @@ signal_add_first("gui key pressed", sub {
 			# match screen coordinates
 			$mouse_xterm_combo[1]--;
 			$mouse_xterm_combo[2]--;
-			# TODO signal_emit("mouse event", $mouse_xterm_combo[0], $mouse_xterm_combo[1], $mouse_xterm_combo[2], $mouse_xterm_previous[0], $mouse_xterm_previous[1], $mouse_xterm_previous[2]);
 			mouse_event($mouse_xterm_combo[0], $mouse_xterm_combo[1], $mouse_xterm_combo[2], $mouse_xterm_previous[0], $mouse_xterm_previous[1], $mouse_xterm_previous[2]);
 		}
 		signal_stop();
@@ -111,23 +112,40 @@ signal_add_first("gui key pressed", sub {
 });
 
 sub UNLOAD {
-	print STDERR "\e[?1000l"; # stop tracking
+	mouse_disable();
 }
 
 if ($ENV{"TERM"} !~ /^rxvt|screen|xterm(-color)?$/) {
 	die "Your terminal doesn't seem to support this.";
 }
 
-print STDERR "\e[?1000h"; # start tracking
+mouse_enable();
 
 Irssi::command("/^bind meta-[M /mouse_xterm"); # FIXME evil
 Irssi::command_bind("mouse_xterm", sub {$mouse_xterm_status = 0;});
+Irssi::command_bind 'mouse' => sub {
+	my ($data, $server, $item) = @_;
+	$data =~ s/\s+$//g;
+	Irssi::command_runsub('mouse', $data, $server, $item);
+};
 
-settings_add_str("lookandfeel", "mouse_gesture_up", "/window last");
-settings_add_str("lookandfeel", "mouse_gesture_down", "/window goto active");
-settings_add_str("lookandfeel", "mouse_gesture_left", "/window prev");
-settings_add_str("lookandfeel", "mouse_gesture_bigleft", "/eval window prev;window prev");
-settings_add_str("lookandfeel", "mouse_gesture_right", "/window next");
-settings_add_str("lookandfeel", "mouse_gesture_bigright", "/eval window next;window next");
+# temporarily disable mouse handling. Useful for copy-pasting
+Irssi::command_bind 'mouse tempdisable' => sub {
+	my ($data, $server, $item) = @_;
+	my $seconds = ($data eq '') ? 5 : $data; # optional argument saying how many seconds, defaulting to 5
+	mouse_disable();
+	Irssi::timeout_add_once($seconds * 1000, 'mouse_enable', undef); # turn back on after $second seconds
+};
+
+for my $button (@BUTTONS) {
+	settings_add_str("lookandfeel", "mouse" . $button . "_click", "/mouse tempdisable 5");
+	settings_add_str("lookandfeel", "mouse" . $button . "_gesture_up", "/window last");
+	settings_add_str("lookandfeel", "mouse" . $button . "_gesture_down", "/window goto active");
+	settings_add_str("lookandfeel", "mouse" . $button . "_gesture_left", "/window prev");
+	settings_add_str("lookandfeel", "mouse" . $button . "_gesture_bigleft", "/eval window prev;window prev");
+	settings_add_str("lookandfeel", "mouse" . $button . "_gesture_right", "/window next");
+	settings_add_str("lookandfeel", "mouse" . $button . "_gesture_bigright", "/eval window next;window next");
+}
+
 settings_add_str("lookandfeel", "mouse_scroll_up", "/scrollback goto -10");
 settings_add_str("lookandfeel", "mouse_scroll_down", "/scrollback goto +10");
